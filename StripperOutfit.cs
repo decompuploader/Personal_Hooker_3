22d6d12ba83a9f8ca57eeb7ef2ecbc2ba072f923
decompuploader <87332511+decompuploader@users.noreply.github.com>
2021-07-12 18:48:26 +0100

    Create StripperOutfit.cs

---
+++ StripperOutfit.cs
@@ -0,0 +1,61 @@
+namespace Personal_Hooker
+{
+  public class StripperOutfit
+  {
+    public bool Set { get; set; }
+
+    public StripperClothes _0 { get; set; }
+
+    public StripperClothes _1 { get; set; }
+
+    public StripperClothes _2 { get; set; }
+
+    public StripperClothes _3 { get; set; }
+
+    public StripperClothes _4 { get; set; }
+
+    public StripperClothes _5 { get; set; }
+
+    public StripperClothes _6 { get; set; }
+
+    public StripperClothes _7 { get; set; }
+
+    public StripperClothes _8 { get; set; }
+
+    public StripperClothes _9 { get; set; }
+
+    public StripperClothes _10 { get; set; }
+
+    public StripperClothes _11 { get; set; }
+
+    public StripperOutfit(
+      bool S,
+      StripperClothes _Cloth0,
+      StripperClothes _Cloth1,
+      StripperClothes _Cloth2,
+      StripperClothes _Cloth3,
+      StripperClothes _Cloth4,
+      StripperClothes _Cloth5,
+      StripperClothes _Cloth6,
+      StripperClothes _Cloth7,
+      StripperClothes _Cloth8,
+      StripperClothes _Cloth9,
+      StripperClothes _Cloth10,
+      StripperClothes _Cloth11)
+    {
+      this.Set = S;
+      this._0 = _Cloth0;
+      this._1 = _Cloth1;
+      this._2 = _Cloth2;
+      this._3 = _Cloth3;
+      this._4 = _Cloth4;
+      this._5 = _Cloth5;
+      this._6 = _Cloth6;
+      this._7 = _Cloth7;
+      this._8 = _Cloth8;
+      this._9 = _Cloth9;
+      this._10 = _Cloth10;
+      this._11 = _Cloth11;
+    }
+  }
+}
